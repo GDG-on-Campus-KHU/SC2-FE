c58b6ee981701ed6150b6d80c2ad86aa2ae56ed6
Jeongwon Kim <mickykim4@gmail.com>
2024-11-29 16:18:02 +0900

    feat: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import 'firebase_options.dart';
 import 'screens/instruction_screen.dart';
 import 'screens/main_screen.dart';
 import 'screens/shelter_screen.dart';
 import 'utils/theme/color_manager.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(child: App()));
 }
 
