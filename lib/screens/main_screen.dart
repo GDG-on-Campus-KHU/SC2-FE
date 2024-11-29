bcfc2bad6df905cac407ff20b1bf58b8519678f9
Jeongwon Kim <mickykim4@gmail.com>
2024-11-29 16:23:27 +0900

    feat: get device token

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/services.dart';
 
 import '../utils/theme/color_manager.dart';
 import 'instruction_screen.dart';
@@ -6,8 +8,21 @@ import 'instruction_screen.dart';
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
+  Future<void> getDeviceToken() async {
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+    print('FCM Token: $fcmToken');
+    // TODO: Send this token to your backend or store it as needed
+  }
+
   @override
   Widget build(BuildContext context) {
+    getDeviceToken();
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: ColorManager.background,
+      systemNavigationBarColor: ColorManager.background,
+    ));
+
     return PopScope(
       canPop: false,
       child: Scaffold(
