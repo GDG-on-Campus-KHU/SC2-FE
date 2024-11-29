8a9275c65a8df5b43e201d3510643e5043b9967c
Jeongwon Kim <mickykim4@gmail.com>
2024-11-30 02:45:16 +0900

    fix: fix api call error

--- lib/utils/service/shelter_service.dart
+++ lib/utils/service/shelter_service.dart
@@ -5,25 +5,26 @@ import 'package:http/http.dart' as http;
 class ShelterService {
   Future<List<ShelterModel>> fetchShelters(String query) async {
     try {
-      // maybe there's no need to encode the query. change in the future if needed, or hard code it.
       final encodedQuery = Uri.encodeComponent(query.trim());
-      final uri = Uri.parse(
-          'http://localhost:8080/api/map/search?query="$encodedQuery"');
+      final uri =
+          Uri.parse('http://localhost:8080/api/map/search?query=$encodedQuery');
+
+      // print('Fetching shelters from: $uri');
 
       final response = await http.get(uri);
+      // print('Response status: ${response.statusCode}');
+      // print('Response body: ${response.body}');
 
       if (response.statusCode == 200) {
-        final List<dynamic> jsonList = json.decode(response.body);
-        return jsonList.map((json) => ShelterModel.fromJson(json)).toList();
+        final Map<String, dynamic> jsonMap = json.decode(response.body);
+        final List<dynamic> sheltersList = jsonMap['items'] ?? [];
+        return sheltersList.map((json) => ShelterModel.fromJson(json)).toList();
       } else {
-        throw Exception('Failed to fetch route: ${response.statusCode}');
-        // print('Error: HTTP ${response.statusCode}');
-        // return [];
+        throw Exception('Failed to fetch shelters: ${response.statusCode}');
       }
     } catch (e) {
-      throw Exception('Error fetching route: $e');
-      // print('Error in searchShelters: $e');
-      // return [];
+      // print('Error in fetchShelters: $e');
+      throw Exception('Error fetching shelters: $e');
     }
   }
 }
